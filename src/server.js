c07e912eed5b0e6dc7afb76259bd3716991f04cb
Nitin <nitin@example.com>
2026-05-18 19:16:01 +0530

    feat: update CORS configuration to allow production frontend URL

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"], // Allow Vite frontend
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://habbit-tracker-virid.vercel.app/"], // Allow Vite frontend
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
   })
